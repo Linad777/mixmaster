8eb0b7ce17a1b073270492b80f7a696d1383ff3b
Daniil Vnukov <danyavnukov1033@gmail.com>
2025-10-27 15:42:06 +0200

    а

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,37 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+import {About, Landing, Error, Cocktail, HomeLayout, Newsletter, SingleError} from "./pages/index.js";
+
+import {loader as landingLoader} from "./pages/Landing.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index:true,
+        element: <Landing />,
+        errorElement: <SingleError/>,
+        loader: landingLoader,
+      },
+      {
+        path: "cocktail/:id",
+        element: <Cocktail />,
+      },
+      {
+        path: "newsletter",
+        element: <Newsletter />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ]
+  },
+])
 const App = () => {
-  return <h2>mixmaster starter</h2>;
+  return <RouterProvider router={router} />
 };
 export default App;
