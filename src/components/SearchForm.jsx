ecef5eb43a890719836adaf5f449e5c822094fb4
Daniil Vnukov <danyavnukov1033@gmail.com>
2025-10-28 10:34:58 +0200

    а

--- src/components/SearchForm.jsx
+++
@@ -1,6 +0,0 @@
-const SearchForm = () => {
-    return (
-        <h2>SearchForm</h2>
-    )
-}
-export default SearchForm;
--- src/pages/Cocktail.jsx
+++
@@ -1,6 +0,0 @@
-const Cocktail = () => {
-    return (
-        <h1>Cocktail</h1>
-    )
-}
-export default Cocktail;
