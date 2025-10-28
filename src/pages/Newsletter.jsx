17cdb70f7fc212c1a47eddad321473048830bde3
Daniil Vnukov <danyavnukov1033@gmail.com>
2025-10-28 12:29:51 +0200

    Some change4

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,6 +1,25 @@
 const Newsletter = () => {
     return (
-        <h1>Newsletter</h1>
+        <form className='form'>
+            <h4 style={{textAlign:'center', marginBottom:'2rem'}}>
+                our newsletter
+            </h4>
+            {/*name*/}
+            <div className='form-row'>
+                <label htmlFor="name" className='form-label'>name</label>
+                <input type='text' name='name' id='name' defaultValue='john' className='form-input' />
+            </div>
+            {/*lastName*/}
+            <div className='form-row'>
+                <label htmlFor="name" className='form-label'>lastName</label>
+                <input type='text' name='lastName' id='lastName' defaultValue='smith' className='form-input' />
+            </div>
+            {/*email*/}
+            <div className='form-row'>
+                <label htmlFor="name" className='form-label'>name</label>
+                <input type='text' name='email' id='email' defaultValue='john' className='form-input' />
+            </div>
+        </form>
     )
 }
 export default Newsletter;
